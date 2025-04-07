2d3dc9b25a4677280c4dc79b1c453ffb30e4bc75
dass <jairocmnetto@gmail.com>
2025-04-07 15:48:21 -0300

    Update atividades.js

--- netlify/functions/atividades.js
+++ netlify/functions/atividades.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // Essas variáveis serão definidas no painel do Netlify
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.DB_HOST,       // Essas variáveis serão definidas no painel do Netlify
+  user: process.DB_USER,
+  password: process.DB_PASSWORD,
+  database: process.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
